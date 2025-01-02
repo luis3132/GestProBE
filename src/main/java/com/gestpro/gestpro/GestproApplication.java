3a0c3957761d19b1d9ab787caa49a39291648d81
luis3132 <luisangoco26@gmail.com>
2025-01-02 13:45:55 -0500

    initialize project structure with Gradle and Spring Boot setup

---
+++ src/main/java/com/gestpro/gestpro/GestproApplication.java
@@ -0,0 +1,13 @@
+package com.gestpro.gestpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestpro/gestpro/GestproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestpro.gestpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
