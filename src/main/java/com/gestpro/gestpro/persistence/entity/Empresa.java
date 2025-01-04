c149090f7a5140fed23d942167e699ab25401c67
luis3132 <luisangoco26@gmail.com>
2025-01-04 12:43:45 -0500

    refactor: remove unused imports in Empresa entity class

--- src/main/java/com/gestpro/gestpro/persistence/entity/Empresa.java
+++ src/main/java/com/gestpro/gestpro/persistence/entity/Empresa.java
@@ -12,10 +12,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -62,5 +59,5 @@ public class Empresa {
     @JsonManagedReference("empresa-plan")
     @JsonIgnoreProperties("empresa")
     private List<VigenciaPlan> vigenciasPlan;
-    
+
 }
